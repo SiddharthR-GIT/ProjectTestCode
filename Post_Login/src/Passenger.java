a88ac27aa23049e9a84bd4e779a00890dbe99d39
SidCodeBanger <32364039+SidCodeBanger@users.noreply.github.com>
2018-05-01 19:11:44 +0100

    Test used in FYP-PICKMEUP

--- Post_Login/src/Passenger.java
+++ Post_Login/src/Passenger.java
@@ -1,26 +1,29 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import java.util.logging.Logger;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Passenger extends HttpServlet{
+@WebServlet(urlPatterns = "/Passenger")
+public class Passenger extends HttpServlet {
+
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        String origin = request.getParameter("Origin");
-        String destination = request.getParameter("Destination");
+        String origin = request.getParameter("OriginIn");
+        String destination = request.getParameter("DestinationIn");
 
         PrintWriter pr = response.getWriter();
         Logger log = Logger.getLogger(Passenger.class.getName());
 
-        try{
-
-        }catch (Exception ee){
-
-        }
-
-
+        pr.println("<html><head><title>PICKMEUP</title></head><body>");
+        pr.println("<p>" + origin + "," + destination + "</p></body></html>");
+        log.info("Getting the values");
+        System.out.print("Passed it");
+        pr.flush();
+        pr.close();
     }
 }
